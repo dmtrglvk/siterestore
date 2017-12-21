4c09eff697f1068eb2ffb613cf2a4cbceb3cd7a8
Dmytro Golovko <dmtrglvk@gmail.com>
2017-12-21 19:49:56 +0200

    working

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,31 @@ $(document).ready(function(){
 		smartSpeed: 1000,
 		mouseDrag: false
 	});
+	accordion();
+	tabs();
+
+	$('.form-item-holder select').styler();
 })
+
+function accordion(){
+	var opener = $('.js-drop-opener'),
+		dropBox = $('.js-drop-opened');
+
+	opener.on('click', function(){
+		$(this).next(dropBox).slideToggle();
+		$(this).toggleClass('active');
+	})
+}
+
+function tabs() {
+	var tabLink = $('.js-tab-menu a'),
+		tabContent = $('.js-tab-content');
+
+	tabLink.on('click', function(e){
+		e.preventDefault();
+		tabContent.removeClass('active');
+		$($(this).attr('href')).addClass('active');
+		tabLink.removeClass('active');
+		$(this).addClass('active');
+	})
+}
