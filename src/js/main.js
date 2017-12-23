7439f7820cb0a436e9712f9eb8dc902cf3181f18
Dmytro Golovko <dmtrglvk@gmail.com>
2017-12-23 15:22:53 +0200

    working responsive

--- src/js/main.js
+++ src/js/main.js
@@ -7,15 +7,17 @@ $(document).ready(function(){
 	});
 	accordion();
 	tabs();
-	headerFixed ();
+
+	$(".header").sticky({
+		className: 'fixed',
+		zIndex: 1001
+	});
+
+	initPopups();
 
 	$('.form-item-holder select').styler();
 	$('.bottom-row input[type="file"]').styler();
 })
-
-$(window).scroll(function(){
-	headerFixed ();
-});
 
 function accordion(){
 	var opener = $('.js-drop-opener'),
@@ -40,16 +42,88 @@ function tabs() {
 	})
 }
 
-function headerFixed () {
-	if ($(window).scrollTop() > 500) {
-		$('.header').addClass('fixed').animate({
-			top: 0
-		}, 500)
+function initPopups() {
+	var opener = $('.open-popup'),
+		lightbox = $('.popup'),
+		fader = $('.fader'),
+		closer = $('.popup .close-popup'),
+		id;
+
+	lightbox.each(function(){
+		var el = $(this);
+		if(el.hasClass('show-me-onload')) {
+			popupPosition(el);
+			el.fadeIn();
+			fader.fadeIn();
+		} else {
+			el.hide();
+		}
+	});
+
+	opener.on('click', function(e){
+		if($(this).parents('.popup')) {
+			$(this).parents('.popup').hide();
+		}
+		id = $(this).attr('href').substr('1');
+		lightbox.each(function(){
+			var lightbox_box = $(this);
+			if(id == $(this).attr('id')) {
+				popupPosition(lightbox_box);
+				lightbox_box.fadeIn();
+				fader.fadeIn();
+			}
+		});
+		e.preventDefault();
+	});
+
+	function disappearLightbox() {
+
+		lightbox.fadeOut();
+		setTimeout(function(){
+			lightbox.removeAttr('style');
+			if($('.page').attr('style')){
+				$('body').addClass('open-menu');
+			}
+		}, 500);
+		setTimeout(function(){
+			fader.fadeOut();
+		}, 100)
+	}
+
+	function closeLightbox(element) {
+		element.click(function(e){
+			disappearLightbox();
+			e.preventDefault();
+		})
+	}
+
+	closeLightbox(fader);
+	closeLightbox(closer);
+
+	$(window).keydown(function(eventObject){
+		if (eventObject.which == 27){
+			disappearLightbox()
+		}
+	});
+
+}
+
+function popupPosition(popup){
+
+	if(popup.height() > $(window).height()) {
+		popup.css({
+			'position': 'absolute',
+			'top': $(window).scrollTop() + 20,
+			'margin-top': 0 + 'px',
+			'tranform': 'translateY(0)'
+
+		});
 	} else {
-		// $('.fixed').animate({
-		// 	top: '-100px'
-		// }, 500, function(){
-		// 	$(this).removeClass('fixed')
-		// })
+		popup.css({
+			'top': '50%',
+			'margin-top':-popup.height()/2 + 'px',
+			'position':'fixed',
+			'tranform': 'translateY(-50%)'
+		});
 	}
 }
